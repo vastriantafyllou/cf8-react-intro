bb3999b72ffe1687c0c9ffa3437093a120a4d81a
vas.triantafyllou <triantafyllou.vasileios@gmail.com>
2025-09-26 18:54:44 +0300

    feat: add component ArrowFunctionalComponentWithPropsType

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,8 @@
 // import ClassComponent from "./components/ClassComponent.tsx";
 // import FunctionalComponent from "./components/FunctionalComponent.tsx";
 // import ArrowFunctionalComponent from "./components/ArrowFunctionalComponent.tsx";
-import ArrowFunctionalComponentWithProps from "./components/ArrowFunctionalComponentWithProps.tsx";
+// import ArrowFunctionalComponentWithProps from "./components/ArrowFunctionalComponentWithProps.tsx";
+import ArrowFunctionalComponentWithPropsType from "./components/ArrowFunctionalComponentWithPropsType.tsx";
 
 function App() {
     return (
@@ -11,8 +12,11 @@ function App() {
       {/*<ClassComponent/>*/}
       {/*<FunctionalComponent/>*/}
       {/*<ArrowFunctionalComponent/>*/}
-      <ArrowFunctionalComponentWithProps title="Is an Arrow Functional Component with Props" />
-      <ArrowFunctionalComponentWithProps title="2nd Title" />
+      {/*<ArrowFunctionalComponentWithProps title="Is an Arrow Functional Component with Props" />*/}
+      {/*<ArrowFunctionalComponentWithProps title="2nd Title" />*/}
+      <ArrowFunctionalComponentWithPropsType
+        title="Is an Arrow Functional Component with 2 Props"
+        description="lorem ipsum dolor sit amet" />
     </>
   )
 }
