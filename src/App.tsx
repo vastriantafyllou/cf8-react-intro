cb3a46ac4acc159a4956f94328b8ebb86346707f
vas.triantafyllou <triantafyllou.vasileios@gmail.com>
2025-09-26 18:42:07 +0300

    feat: add arrow functional component with props

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,18 @@
 // import ViteIntro from "./components/ViteIntro/ViteIntro.tsx";             // ./ σημαίνει ότι είομαστε στο src, και του λέμε να μας φέρει το ViteIntro
-import ClassComponent from "./components/ClassComponent.tsx";
-import FunctionalComponent from "./components/FunctionalComponent.tsx";
-import ArrowFunctionalComponent from "./components/ArrowFunctionalComponent.tsx";
+// import ClassComponent from "./components/ClassComponent.tsx";
+// import FunctionalComponent from "./components/FunctionalComponent.tsx";
+// import ArrowFunctionalComponent from "./components/ArrowFunctionalComponent.tsx";
+import ArrowFunctionalComponentWithProps from "./components/ArrowFunctionalComponentWithProps.tsx";
 
 function App() {
     return (
     <>
       {/*<ViteIntro />*/}
-      <ClassComponent/>
-      <FunctionalComponent/>
-      <ArrowFunctionalComponent/>
+      {/*<ClassComponent/>*/}
+      {/*<FunctionalComponent/>*/}
+      {/*<ArrowFunctionalComponent/>*/}
+      <ArrowFunctionalComponentWithProps title="Is an Arrow Functional Component with Props" />
+      <ArrowFunctionalComponentWithProps title="2nd Title" />
     </>
   )
 }
---
+++ src/components/ArrowFunctionalComponentWithProps.tsx
@@ -0,0 +1,14 @@
+
+type Props = {
+  title: string;
+}
+
+const ArrowFunctionalComponentWithProps =({title}: Props) => {
+  return (
+    <>
+      <h1 className="text-center text-xl font-bold mt-12">{title}</h1>
+    </>
+  )
+}
+
+export default ArrowFunctionalComponentWithProps
