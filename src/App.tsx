ca0cc873b1f8d2f3c6b37870969ffc6d7ad17e2b
vas.triantafyllou <triantafyllou.vasileios@gmail.com>
2025-09-26 12:17:14 +0300

    chore: corrected typo FunctionComponent to FunctionalComponent

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 // import ViteIntro from "./components/ViteIntro/ViteIntro.tsx";             // ./ σημαίνει ότι είομαστε στο src, και του λέμε να μας φέρει το ViteIntro
 import ClassComponent from "./components/ClassComponent.tsx";
-import FunctionComponent from "./components/FunctionalComponent.tsx";
+import FunctionalComponent from "./components/FunctionalComponent.tsx";
 import ArrowFunctionalComponent from "./components/ArrowFunctionalComponent.tsx";
 
 function App() {
@@ -8,7 +8,7 @@ function App() {
     <>
       {/*<ViteIntro />*/}
       <ClassComponent/>
-      <FunctionComponent/>
+      <FunctionalComponent/>
       <ArrowFunctionalComponent/>
     </>
   )
--- src/components/FunctionalComponent.tsx
+++ src/components/FunctionalComponent.tsx
@@ -1,5 +1,5 @@
-function FunctionComponent(){
+function FunctionalComponent(){
     const title = "Is a Functional Component";
     return <h1 className="text-center text-xl font-bold mt-12">{title}</h1>
 }
-export default FunctionComponent;
+export default FunctionalComponent;
