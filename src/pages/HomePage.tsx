8e3cef91b7444104a2262c8d3ca88aec14d7127f
vas.triantafyllou <triantafyllou.vasileios@gmail.com>
2025-11-13 22:59:59 +0200

    feat: add multifield forms with hook, validation & zod components

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -9,6 +9,9 @@ const HomePage = () => {
     {path: "/examples/controlled-input", label: "Controlled Input"},
     {path: "/examples/uncontrolled-input", label: "Uncontrolled Input"},
     {path: "/examples/multifield-form", label: "Multifield Form Example"},
+    {path: "/examples/multifield-form-validation", label: "Multifield Form With Validation Example"},
+    {path: "/examples/multifield-form-zod", label: "Multifield Form With Zod Example"},
+    {path: "/examples/multifield-form-hook", label: "Multifield Form With Hook Form Example"},
   ];
 
   useEffect(() => {
